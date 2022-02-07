cd8e164dbaa8369711c36672ca2886c43b132e2e
Abdulmuaz Alshaikhli <abdu0445@edu.easj.dk>
2022-02-07 14:42:26 +0100

    This is my vue Project

---
+++ src/components/Component1.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+<component-2></component-2>
+</div>
+    
+</template>
+
+<script>
+import Component2 from './Component2.vue'
+export default {
+    components: {
+        Component2
+    }
+}
+</script>
